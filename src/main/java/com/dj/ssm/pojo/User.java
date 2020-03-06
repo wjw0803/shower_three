952664fff748b6e6fda58675e91553b94c6d1900
zhangbt <zbt1852400187@163.com>
2020-03-06 19:07:47 +0800

    测试了下

--- src/main/java/com/dj/ssm/pojo/User.java
+++ src/main/java/com/dj/ssm/pojo/User.java
@@ -1,11 +1,25 @@
 package com.dj.ssm.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
+@TableName("user")
 public class User {
 
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    private String userName;
+
+    private String phone;
+
+    private String email;
+
+    //提交的呢
 
 }
