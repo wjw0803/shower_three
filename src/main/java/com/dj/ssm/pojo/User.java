822f121b02d1ef109de00801703b205730020a2e
wjw <w18835460636@163.com>
2020-03-06 19:14:16 +0800

    测试能否提交

--- src/main/java/com/dj/ssm/pojo/User.java
+++ src/main/java/com/dj/ssm/pojo/User.java
@@ -6,6 +6,9 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+/**
+ * @author dj这是和波涛的测试类
+ */
 @Data
 @Accessors(chain = true)
 @TableName("user")
@@ -21,5 +24,9 @@ public class User {
     private String email;
 
     //提交的呢
+    private String ceshi = "6666";
+
+    private boolean  a = false;
+
 
 }
